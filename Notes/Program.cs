faf6b76f61125f9c7ff2caedcfefa038cfbce7e9
d3c6e1 <dmytro.koval@nure.ua>
2020-08-28 21:43:33 +0300

    data generation

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Notes.Models;
 
 namespace Notes
 {
@@ -13,7 +15,17 @@ namespace Notes
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<NotesContext>();
+
+                DataGenerator.Initialize(services);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
